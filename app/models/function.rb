280126006ec4e1245186b67d6ea1093b46495e7a
Алдияр Примбаев <primbayev.96aldi@gmail.com>
2019-01-16 18:11:51 +0600

    Добавил валидации для function

--- app/models/function.rb
+++ app/models/function.rb
@@ -1,3 +1,8 @@
 class Function < ApplicationRecord
   belongs_to :role
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :model_name, presence: true, length: { maximum: 50 }
+  validates :description, presence: true, length: { maximum: 250 }
+
 end
