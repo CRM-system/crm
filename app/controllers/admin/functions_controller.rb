06a0e648f3ed00991ada14c7419e19bb34c3515f
Алдияр Примбаев <primbayev.96aldi@gmail.com>
2019-01-16 17:58:30 +0600

    Начал создавать функционал по созданию function

--- app/controllers/admin/functions_controller.rb
+++ app/controllers/admin/functions_controller.rb
@@ -4,5 +4,28 @@ class Admin::FunctionsController < AdminController
     @functions = Function.all
   end
 
+  def new
+    @function = Function.new
+  end
 
+  def create
+    @function = Function.new(function_params)
+    if @function.save
+      redirect_to admin_functions_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+    def function_params
+      params.require(:function).permit(
+        :name,
+        :functionable_id,
+        :functionable_type,
+        :access,
+        :role_id
+      )
+    end
 end
