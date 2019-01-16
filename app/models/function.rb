41c13342273cf2f01f827afa2dbd9c9213c3c5a5
Алдияр Примбаев <primbayev.96aldi@gmail.com>
2019-01-16 19:22:01 +0600

    Сделал поля name и model уникальными

--- app/models/function.rb
+++ app/models/function.rb
@@ -2,7 +2,8 @@ class Function < ApplicationRecord
   belongs_to :role
 
   validates :name, presence: true, length: { maximum: 50 }
-  validates :model_name, presence: true, length: { maximum: 50 }
+  validates :model, presence: true, length: { maximum: 50 }
   validates :description, presence: true, length: { maximum: 250 }
+  validates_uniqueness_of :model, scope: :name
 
 end
