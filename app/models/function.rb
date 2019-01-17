14483070c5e08fd9f688ce8a0d70e5aacd464d33
Алдияр Примбаев <primbayev.96aldi@gmail.com>
2019-01-17 14:43:58 +0600

    Написал метод который делает все буквы у двух полей модели маленькими

--- app/models/function.rb
+++ app/models/function.rb
@@ -1,9 +1,17 @@
 class Function < ApplicationRecord
+  before_validation :downcase
+
   belongs_to :role
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :model, presence: true, length: { maximum: 50 }
   validates :description, presence: true, length: { maximum: 250 }
   validates_uniqueness_of :model, scope: :name
+
+  private
+    def downcase
+      model.downcase!
+      name.downcase!
+    end
 
 end
