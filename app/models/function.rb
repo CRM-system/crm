755af6a5679e1c66d39e7c96cb4f65d83088cef1
Алдияр Примбаев <primbayev.96aldi@gmail.com>
2019-01-17 14:53:17 +0600

    Немного улучшил валидацию

--- app/models/function.rb
+++ app/models/function.rb
@@ -6,7 +6,7 @@ class Function < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
   validates :model, presence: true, length: { maximum: 50 }
   validates :description, presence: true, length: { maximum: 250 }
-  validates_uniqueness_of :model, scope: :name
+  validates_uniqueness_of :model, scope: [:name, :role_id]
 
   private
     def downcase
