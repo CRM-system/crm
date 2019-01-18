cc4a9b9cf3d7d20cd5fbeafaee05566bf726eb13
Алдияр Примбаев <primbayev.96aldi@gmail.com>
2019-01-18 17:59:59 +0600

    Сделал pull из master

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
 	validates :name, presence: true, length: { maximum: 150 }
 	validates :price, presence: true
 	validates :description, presence: true, length: {maximum: 200}
-	
+
 	has_one_attached :picture
 
 	after_validation :if_picture_not_valid_remove_it
