230f3b750dd6ca1598cc771646a6828fce350d32
Айбек Абенов <abenovaybek@gmail.com>
2019-01-21 18:38:33 +0600

    Убрал комменты

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,16 +8,9 @@ class Product < ApplicationRecord
 	validates :picture,
 	file_content_type: { allow: ['image/jpeg', 'image/png'] },
 	if: -> { picture.attachment.present? }
-	# validate :correct_picture_type
+
 	after_validation :if_picture_not_valid_remove_it
 
-	# def correct_picture_type
-	# 	if picture.attached? && !picture.content_type.in?(%w(image/jpeg image/png))
-	# 		errors.add(:picture, 'must be a JPEG or JPG or PNG')
-	# 	elsif picture.attached? == false
-	# 		errors.add(:picture, 'must have an avatar attached')
-	# 	end
-	# end
 
 		def if_picture_not_valid_remove_it
 			self.picture.purge if self.errors.present? && self.errors.details.key?(:picture)
