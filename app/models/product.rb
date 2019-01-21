647b1a2f86aa521b982a3df3a20d1d6e974674dc
Данил Шпаков <danilyh7@gmail.com>
2019-01-21 19:46:24 +0600

    Добавил дизайн в проект

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,17 +1,6 @@
 class Product < ApplicationRecord
+	has_one_attached :picture
 	validates :name, presence: true, length: { maximum: 150 }
 	validates :price, presence: true
 	validates :description, presence: true, length: {maximum: 200}
-
-	has_one_attached :picture
-
-	after_validation :if_picture_not_valid_remove_it
-
-	validates :picture,
-	file_content_type: { allow: ['image/jpeg', 'image/png'] },
-	if: -> { picture.attachment.present? }
-
-	def if_picture_not_valid_remove_it
-		self.picture.purge if self.errors.present? && self.errors.details.key?(:picture)
-	end
 end
