03ee42ccf115c9d51393e685deef71bb64987402
Алдияр Примбаев <primbayev.96aldi@gmail.com>
2019-01-24 15:20:24 +0600

    Была ошибка связанная с тем что в статус :new нельзя было использовать. Ошибка исправленна

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ApplicationRecord
-	enum status: [ :new, :processed, :problematic, :completed ]
+	enum status: [ :news, :processed, :problematic, :completed ]
 
   belongs_to :product
 
@@ -10,9 +10,9 @@ class Order < ApplicationRecord
   validates :client_phone, presence: true, length: { maximum: 12 }
   validates :client_email, format: { with: URI::MailTo::EMAIL_REGEXP },
   					length: { maximum: 100 }
-  validates :client_addres, presence: true, length: { maximum: 250 } 
+  validates :client_addres, presence: true, length: { maximum: 250 }
   validates :delivery_type, presence: true, length: { maximum: 250 }
   validates :order_price, presence: true, numericality: { only_integer: true }
   validates :quantity, presence:true, numericality: { only_integer: true }
-  validates :total_price, presence:true, numericality: { only_integer: true } 
+  validates :total_price, presence:true, numericality: { only_integer: true }
 end
