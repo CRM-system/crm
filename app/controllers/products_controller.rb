57141dd7bfd5ec3ef829d9f581f97b5c75b55dc1
Алдияр Примбаев <primbayev.96aldi@gmail.com>
2019-01-25 15:49:48 +0600

    Убрал лишнее

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,21 +4,6 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
-  def send_to_order
-    @product = Product.find(params[:id])
-    @order = Order.create!(
-      product_id: @product.id,
-      delivery_type: 'Выяснить',
-      client_name: 'Выяснить',
-      client_addres: 'Выяснить',
-      order_price: 0,
-      quantity: 0,
-      total_price: 0
-    )
-
-    redirect_to @product
-  end
-
   def index
     @products = Product.all
   end
