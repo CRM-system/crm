b1934098f4c0c3ddb5975423bb456c8c423e4844
Алдияр Примбаев <primbayev.96aldi@gmail.com>
2019-01-25 15:47:25 +0600

    Поменял валидации

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,8 +3,8 @@ class Order < ApplicationRecord
 
   belongs_to :product
 
-  # before_validation { client_name.capitalize! }
-  # before_validation { email.downcase! }
+  before_validation { client_name.capitalize! }
+  before_validation { client_email.downcase! }
 
   validates :client_name, presence: true, length: { maximum: 50 }
   validates :client_phone, presence: true, length: { maximum: 12 }
@@ -12,7 +12,7 @@ class Order < ApplicationRecord
   					length: { maximum: 100 }
   validates :client_addres, presence: true, length: { maximum: 250 }
   validates :delivery_type, presence: true, length: { maximum: 250 }
-  validates :order_price, presence: true, numericality: { only_integer: true }
-  validates :quantity, presence:true, numericality: { only_integer: true }
-  validates :total_price, presence:true, numericality: { only_integer: true }
+  validates :order_price, presence: true, numericality: { greater_than: 0 }
+  validates :quantity, presence:true, numericality: { greater_than: 0 }
+  validates :total_price, presence:true, numericality: { greater_than: 0 }
 end
