f001647ed8cb6c1ba07e2428fdbf62af51f2778d
Kairat Symanov <symanov79@gmail.com>
2019-02-05 21:04:48 +0600

    #52 Реализовал тикет, также изменил кнопки, добавил стили

--- app/models/role.rb
+++ app/models/role.rb
@@ -2,7 +2,7 @@ class Role < ApplicationRecord
 	validates :name, presence: true, length: { maximum: 50 }
 
 	has_many :workers
-  has_many :functions, dependent: :destroy
-  has_many :role_order_statuses
-  has_many :order_statuses, :through => :role_order_statuses
+	has_many :functions, dependent: :destroy
+	has_many :role_order_statuses
+	has_many :order_statuses, :through => :role_order_statuses
 end
