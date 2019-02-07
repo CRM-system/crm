96539d665390f84c07abdcd642427cbc679caef1
Данил Шпаков <danilyh7@gmail.com>
2019-02-07 16:55:03 +0600

    Рефакторинг контроллера admin/roles/controller

--- config/routes.rb
+++ config/routes.rb
@@ -19,10 +19,5 @@ end
 		resources :products
 		post '/duplicate/products/:id' => 'products#duplicate', as: 'duplicate'
 		resources :orders
-		# get '/status' => 'orders#status', as:'status'
-		# post '/change_status/orders/:id/:status' => 'orders#change_status', as: 'change_status'
-		# get "/order_by_status", to: "orders#get_orders_by_status_params", as: 'order_by_status'
-		# post "/change_status_from_new_to_refused/:id" => "orders#change_status_from_new_to_refused", as: 'from_new_to_refused'
-
 	end
 end
