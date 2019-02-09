0f61ba01e28c7ae8f654bce626ab01cc7b9e1666
Алдияр Примбаев <primbayev.96aldi@gmail.com>
2019-02-09 16:23:44 +0600

    #56 Сделал способ доставки с попомощью enum

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,6 +4,9 @@ class Order < ApplicationRecord
                 :assemblage, :prepared, :sent, :handed_in,
                 :handed_and_paid, :return, :refund_received, :delivered,
                 :reminder_1, :reminder_2, :wanted]
+
+  enum delivery_type: [:courier, :mail, :self_delivery]
+
   translate_enum :status
   belongs_to :product
 
@@ -14,11 +17,10 @@ class Order < ApplicationRecord
   validates :client_phone, presence: true, length: { maximum: 12 }
   #validates :client_email, format: { with: URI::MailTo::EMAIL_REGEXP },length: { maximum: 100 }
   validates :client_addres, presence: true, length: { maximum: 250 }
-  validates :delivery_type, presence: true, length: { maximum: 250 }
   validates :order_price, presence: true, numericality: { greater_or_equal_to: 0 }
   validates :quantity, presence:true, numericality: { greater_or_equal_to: 0 }
   validates :total_price, presence:true, numericality: { greater_or_equal_to: 0 }
 
   include PgSearch
-  
+
 end
