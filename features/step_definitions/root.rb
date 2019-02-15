ac2936b3d824b6c53dafe7fcb30b27b6c962ec4d
Алдияр Примбаев <primbayev.96aldi@gmail.com>
2019-02-15 18:57:23 +0600

    #60 перенес sleep на последний шаг

--- features/step_definitions/root.rb
+++ features/step_definitions/root.rb
@@ -4,11 +4,11 @@ end
 
 Если("нажимаем на кнопку {string}") do |log_in|
   click_link("#{log_in}")
-  sleep(2)
 end
 
 То("происходит переход на страницу авторизации") do
   url = URI.parse(current_url).path
-
   expect(page).to have_current_path(url)
+
+  sleep(2)
 end
