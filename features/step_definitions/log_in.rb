553775b3a9ad7979980f90aa4d07b8c7de4adf26
Алдияр Примбаев <primbayev.96aldi@gmail.com>
2019-02-15 20:05:48 +0600

    #61 переписал шаги capybara. Добавил проверку авторизации как оператор и директор

--- features/step_definitions/log_in.rb
+++ features/step_definitions/log_in.rb
@@ -1,17 +1,62 @@
-Допустим("Заходим на страницу авторизации") do
+Допустим("Админ заходит на страницу авторизации") do
   visit('/workers/sign_in')
 end
 
-Если("залогинен пользователь с email {string} и паролем {string}") do |email, password|
+Если("залогинен админ с email {string} и паролем {string}") do |email, password|
   within('#new_worker') do
     fill_in('Email', with: email)
     fill_in('Password', with: password)
     click_button('Log in')
   end
+
+  sleep(2)
 end
 
-То("происходит переход на страницу {string}") do |url|
+То("админа переносят на страницу {string}") do |url|
   expect(page).to have_current_path(url)
+end
+
+
+Допустим("Оператор заходит на страницу авторизации") do
+  visit('/workers/sign_in')
+end
+
+Если("залогинен оператор с email {string} и паролем {string}") do |email, password|
+  within('#new_worker') do
+    fill_in('Email', with: email)
+    fill_in('Password', with: password)
+    click_button('Log in')
+  end
+
+  sleep(2)
+end
+
+То("оператора переносят на страницу {string}") do |url|
+  expect(page).to have_current_path(url)
+end
+
+
+Допустим("Директор заходит на страницу авторизации") do
+  visit('/workers/sign_in')
+end
+
+Если("залогинен директор с email {string} и паролем {string}") do |email, password|
+  within('#new_worker') do
+    fill_in('Email', with: email)
+    fill_in('Password', with: password)
+    click_button('Log in')
+  end
+
+  sleep(2)
+end
+
+То("директора переносят на страницу {string}") do |url|
+  expect(page).to have_current_path(url)
+end
+
+
+Допустим("Заходим на страницу авторизации") do
+  visit('/workers/sign_in')
 end
 
 Если("пытаемся залогиниться с email {string} и паролем {string}") do |email, password|
@@ -20,6 +65,8 @@ end
     fill_in('Password', with: password)
     click_button('Log in')
   end
+
+  sleep(2)
 end
 
 То("мы остаемся на странице {string}") do |url|
