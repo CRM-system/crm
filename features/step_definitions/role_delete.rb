2b5c65e7c6088a1ce23335a04e985d081e410160
Алдияр Примбаев <primbayev.96aldi@gmail.com>
2019-02-20 19:03:20 +0600

    #62 Написал шаги для приемочного тестирования, по проверке удаления должностей

---
+++ features/step_definitions/role_delete.rb
@@ -0,0 +1,18 @@
+Если("он удаляет должность с названием {string}") do |title|
+  visit('admin/roles')
+
+  unless find_role(title).nil?
+    role_id = find_role(title).id
+    click_link("role/#{role_id}")
+
+    page.driver.browser.switch_to.alert.accept
+  end
+end
+
+То("{string} не видно в списке должностей") do |title|
+  expect(page).to have_no_content "#{title}"
+end
+
+def find_role(title)
+  Role.find_by(name: "#{title}")
+end
