93d9a485f2212c267f56f6e250ff87102fa96b06
Данил Шпаков <danilyh7@gmail.com>
2019-02-21 23:41:52 +0600

    Сделала рефакторинг orders/controller, написал тест на создание сотрудника

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -10,17 +10,6 @@ class Admin::OrdersController < AdminController
     @order.save
   end
 
-  # def change_status_from_new_to_refused
-  #   @order = Order.find(params[:id])
-  #   @order.update(status: :refused)
-  #   redirect_to admin_orders_path
-  # end
-
-  # def change_status
-  #     @order = Order.find(params[:id])
-  #     @order = Order.update(status: params[:status])
-  # end
-
   def new
     @order = Order.new
   end
@@ -55,67 +44,40 @@ class Admin::OrdersController < AdminController
   end
 
   def check_params
-    start_date =  params[:query][:start_date].to_date
+    start_date = params[:query][:start_date].to_date
+    end_date = get_end_date(params[:query])
+    @orders = get_orders_by_date(start_date, end_date)
+    params[:query].tap{|param| param.delete(:start_date)}
+    params[:query].tap{|param| param.delete(:end_date)}
+    search_by_type(params[:query])
+    render :index
+  end
+
+  def get_end_date(params_query)
     if params[:query][:end_date] == " "
       end_date == Date.today
     else
       end_date =  params[:query][:end_date].to_date
     end
-    @orders = Order.where(created_at: start_date.beginning_of_day..Date.today.beginning_of_day)
-    params[:query].tap{|param| param.delete(:start_date)}
-    params[:query].tap{|param| param.delete(:end_date)}
-    params[:query].each do |search_type, search_type_value|
-      binding.pry
+  end
+
+  def get_orders_by_date(start_date, end_date)
+    if Order.where(params[:query][:created_at]).present?
+      return Order.where(created_at: start_date.beginning_of_day..Date.today.beginning_of_day)
+    else
+      return Order.all
+    end
+  end
+
+  def search_by_type(params_query)
+    params_query.each do |search_type, search_type_value|
       if search_type == "search_all" && search_type_value.present?
-        @orders = Order.search_all("#{params[:query][:search_all]}")
+        return @orders = Order.search_all("#{params[:query][:search_all]}")
       else
-        @orders = @orders.where(search_type => search_type_value) if search_type_value.present?
+        return @orders = @orders.where(search_type => search_type_value) if search_type_value.present?
       end
     end
-    render :index
   end
-
-  # def check_params
-  #   start_date =  params[:query][:start_date].to_date
-  #   if params[:query][:end_date] == " "
-  #     end_date == Date.today
-  #   else
-  #     end_date =  params[:query][:end_date].to_date
-  #   end
-  #   @orders = Order.where(created_at: start_date.beginning_of_day..Date.today.beginning_of_day)
-  #   params[:query].tap{|param| param.delete(:start_date)}
-  #   params[:query].tap{|param| param.delete(:end_date)}
-  #   params[:query].each do |key, value|
-  #     @orders = @orders.where(key => value) if value.present?
-  #   end
-  #   render :index
-  # end
-
-  # def check_params
-  #   @orders = Order.all
-  #   params[:query].each do |key, value|
-  #     search_by_+key if value.present?
-  #   end
-  #   render :index
-  # end
-
-  def search_by_date
-    @orders = Order.where("created_at::date = ?", Date.today)
-  end
-
-  # def check_params
-  #   # @orders = Order.where("created_at: >=?", 5.day.ago)
-  #   @orders = Order.all
-  #   if params[:query][:search_all].present?
-  #     @orders = Order.search_all("#{params[:query][:search_all]}")
-  #   else
-  #     params[:query].except("search_all").each do |key, value|
-  #       @orders = Order.where(key => value) if value.present?
-  #     end
-  #   end
-  #   # binding.pry
-  #   render :index
-  # end
 
   def show
   end
@@ -130,7 +92,6 @@ class Admin::OrdersController < AdminController
   def set_order
     @order = Order.find(params[:id])
   end
-
 
   def return_params
     params[search_type, search_value].each do |key, value|
---
+++ features/step_definitions/workers.rb
@@ -0,0 +1,23 @@
+
+Допустим("залогиненный пользователь с email {string} и паролем {string}") do |email, password|
+  visit("workers/sign_in")
+  fill_in('Email', with: email)
+  fill_in('Password', with: password)
+  click_button('Log in')
+end
+
+Если("он создаёт сотрудника с данными: имя {string} email {string} паролем {string}") do |nickname, email, password|
+  visit("admin/workers/new")
+  within('#new_worker') do
+    fill_in('worker_nickname', with: nickname)
+    fill_in('worker_email', with: email)
+    fill_in('worker_password', with: password)
+    select('Оператор', from: 'worker_role_id')
+    click_button('Создать сотрудника')
+  end
+end
+
+То("сотрудника Серик видно в списке сотрудников") do
+  visit("admin/workers")
+  find('td', text: 'Серик', match: :prefer_exact)
+end
