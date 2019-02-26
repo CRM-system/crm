cd9c251d681bdc6712f2e5cf49baf67aed022f48
Kairat Symanov <symanov79@gmail.com>
2019-02-26 17:02:24 +0600

    #69 приёмочное тестирование ссылок 'Заказы', также добавил к ссылкам id-шки app/views/admin/orders/index.html.erb

---
+++ features/step_definitions/order_links.rb
@@ -0,0 +1,29 @@
+Допустим("пользователь с email {string} и паролем {string}") do |email, password|
+  visit("admin/orders")
+end
+
+Если("он жмёт на ссылку {string}") do |order_all|
+  find('#all_orders').click
+end
+
+То("заказы в со статусом {string} видно в таблице") do |order_all|
+  # find('th', text: status, match: :prefer_exact) 
+  page.has_content?('order_all')
+end
+
+Допустим("администратор жмёт на ссылку {string}") do |status|
+  click_link("#{status}")
+end
+
+То("увидит в таблице заказы со статусом {string}") do |status|
+  find('th', text: status, match: :prefer_exact)
+    sleep(1)
+end
+
+# private
+
+# def find_all_order
+#   Order.count
+# end
+
+
