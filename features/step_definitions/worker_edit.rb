0ab521a1fae9ee488d9eb4a8a361d929a11a4eb3
Dan <danilyh7@gmail.com>
2019-03-05 01:24:34 +0600

    # 63 Пофиксил тест на редактирование сотрудника

--- features/step_definitions/worker_edit.rb
+++ features/step_definitions/worker_edit.rb
@@ -2,10 +2,11 @@
 Допустим("есть администратор") do
 end
 
-Если("админ изменит данные сотрудника Серик на Берик") do
-  visit('admin/workers/4/edit')
+Если("он меняет имя сотрудника с {string} на {string}") do |current_name, new_name|
+  worker_id = find_worker(current_name).id
+  visit("admin/workers/#{worker_id}/edit")
   within('#new_worker') do
-    fill_in('worker_nickname', with: "Берик")
+    fill_in('worker_nickname', with: new_name)
     fill_in('worker_email', with: "beric@mail.com")
     fill_in('worker_password', with: 'qweasd')
     click_button('Подтвердить')
@@ -15,4 +16,10 @@ end
 То("сотрудника Берик видно в списке сотрудников") do
   visit("admin/workers")
   find('td', text: 'Берик', match: :prefer_exact)
+end
+
+private
+
+def find_worker(nickname)
+  Worker.find_by(nickname: "#{nickname}")
 end
