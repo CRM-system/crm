10c3b47b6c23a949c49a77228e5b4069f2553c8e
Dan <danilyh7@gmail.com>
2019-03-19 18:24:10 +0600

    Изменил настройки тестов на использование mazila

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,9 @@ set :deploy_to, "/home/deploy_rails/www"
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml"
+append :linked_files, "config/database.yml", "config/secrets.yml"
+
+
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
